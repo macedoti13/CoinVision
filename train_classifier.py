dcff0743f34ca50781733903d6c3db83206e66db
Thiago Macedo <thialmaceod@gmail.com>
2023-05-15 18:05:46 -0300

    correct path

--- train_classifier.py
+++ train_classifier.py
@@ -2,7 +2,7 @@ from sklearn.ensemble import RandomForestClassifier
 import pickle
 
 # Load the training data
-with open('training_data.pkl', 'rb') as f:
+with open('files/training_data.pkl', 'rb') as f:
     training_data = pickle.load(f)
 
 # Separate features and labels
@@ -16,5 +16,5 @@ model = RandomForestClassifier(n_estimators=100)
 model.fit(features, labels)
 
 # Save the model
-with open('coin_classifier.pkl', 'wb') as f:
+with open('files/coin_classifier.pkl', 'wb') as f:
     pickle.dump(model, f)
