18b065145d20f4dcd8f42ab79d9aea919108068e
Thiago Macedo <thialmaceod@gmail.com>
2023-05-15 14:57:35 -0300

    trains model if not already trained

--- production.py
+++ production.py
@@ -1,25 +1,61 @@
-import matplotlib.pyplot as plt
 from functions import *
+import os
+import subprocess
 import pickle
 
 def main():
-    fig, axs = plt.subplots(2, 5, figsize=(24,10))
-    axs = axs.flatten()
+    model_filename = 'coin_classifier.pkl'
+    training_data_filename = 'training_data.pkl'
+    
+    labels = [
+        ['1_real'],
+        ['50_cents'],
+        ['25_cents'],
+        ['5_cents'],
+        ['10_cents'],
+        ['50_cents', '25_cents', '1_real', '5_cents', '10_cents'],
+        ['50_cents', '25_cents', '10_cents'],
+        ['1_real', '50_cents'],
+        ['1_real', '50_cents', '10_cents'],
+        ['25_cents', '10_cents']
+    ]
+
+    # Check if the model file exists
+    if not os.path.exists(model_filename):
+        # If the model file does not exist, collect all coins and create training data
+        training_data = []
+
+        for i in range(10):
+            img_gray, img_rgb = prepare_img(f'images/img{i}.png')
+            circles = find_circles(img_gray)
+            circles = treat_circles(circles)
+            
+            if circles is not None:
+                filtered_circles = filter_inner_circles(circles, 10)
+                coins_histograms = create_histogram_of_visual_words(img_rgb, filtered_circles)
+                update_training_data(coins_histograms, labels[i], training_data)
+        
+        save_training_data(training_data, training_data_filename)
+
+        # Run the train_classifier.py script to train the model
+        subprocess.run(['python', 'train_classifier.py'])
+    
+    # Load the trained model
+    with open(model_filename, 'rb') as f:
+        model = pickle.load(f)
 
     for i in range(10):
         img_gray, img_rgb = prepare_img(f'images/img{i}.png')
         circles = find_circles(img_gray)
         circles = treat_circles(circles)
-
+        
         if circles is not None:
             filtered_circles = filter_inner_circles(circles, 10)
-            img_rgb = draw_circles(img_rgb, filtered_circles)
+            coins_histograms = create_histogram_of_visual_words(img_rgb, filtered_circles)
 
-        axs[i].imshow(img_rgb, cmap='gray')
-        axs[i].axis('off')
+            # Predict the class of each coin
+            predictions = model.predict(coins_histograms)
+            
+            print(f'Predictions for image {i}: {predictions}')
 
-    plt.tight_layout()
-    plt.show()
-
-if __name__ == "__main__":
-    main()
+main()
